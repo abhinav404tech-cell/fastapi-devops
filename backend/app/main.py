d838fe0b3e5e062e872c695ac0707f8e837680d4
abhinav404tech-cell <abhinav404tech@gmail.com>
2025-12-11 12:44:28 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 # app/main.py
 from fastapi import FastAPI
-
+print("app started")
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    print("read root")
     return {"message": "Hello from FastAPI on ECS!"}
 
 
